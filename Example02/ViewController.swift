9d30098dc1b816c552fe72f61103b08484f7ef98
bruyko-e <bruykoev@gmail.com>
2024-09-06 21:56:01 -0600

    Initial Commit

---
+++ Example02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example02
+//
+//  Created by brubru on 06.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
